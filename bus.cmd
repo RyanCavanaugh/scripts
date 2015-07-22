6144c4936079e7485f7551d0b042d1e8eb38f028
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2015-07-21 17:11:07 -0700

    When's the bus?

---
+++ bus.cmd
@@ -0,0 +1,1 @@
+@powershell -Command bus.ps1
---
+++ bus.ps1
@@ -0,0 +1,10 @@
+$content = (wget 'http://pugetsound.onebusaway.org/where/standard/stop.action?id=1_71334&route=40_100236').Content
+$rgx = [regex]"\<span\>([0-9]+|NOW)\<\/span\>"
+
+$matches = $rgx.Matches($content)
+
+$len = $matches.Captures.Count
+
+for($i = 0; $i -lt $len; $i++) {
+  '>> ' + $matches.Captures[$i].Groups[1].Value
+}
