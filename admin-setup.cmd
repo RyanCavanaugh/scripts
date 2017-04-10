c214797fffe901253896c195e1b3188949d58798
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2017-04-10 16:17:24 -0700

    Move some stuff

--- admin-setup.cmd
+++ admin-setup.cmd
@@ -1,5 +1,8 @@
 REM Allow running Powershell scripts usefully
 powershell /Command Set-ExecutionPolicy RemoteSigned
 
-REM Set up .gitconfig symlink
+REM Set up .gitignore symlink
 mklink %USERPROFILE%\.gitconfig %1\.gitconfig
+mklink %USERPROFILE%\.gitignore %1\.gitignore
+mklink /D %USERPROFILE%\hooks %1\hooks
+
